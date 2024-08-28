a02887bf8786b3b292f8455e959c83bf4a715c14
xk <xv_k@outlook.com>
2024-08-28 11:55:00 +0200

    worked further on it

--- boid.gd
+++ boid.gd
@@ -5,8 +5,7 @@ extends CharacterBody2D
 
 @onready var ray_folder := $RayFolder.get_children()
 var boids_in_range := []
-var boid_velocity := Vector2.ZERO
-var boid_speed := 7.0
+var boid_speed := 100.0
 var screen_size : Vector2
 var movv := 48
 
@@ -15,10 +14,32 @@ var direction: Vector2
 func _ready():
 	screen_size = get_viewport_rect().size
 	randomize()
-	direction = Vector2.RIGHT
+	direction = Vector2(randf_range(-1,1),randf_range(-1,1))
+	print(direction)
 	collision_polygon_2d.polygon = polygon_2d.polygon
 	
 
-func _physics_process(delta):
-	position = position * direction * delta
+func _physics_process(_delta):
+	velocity = direction * boid_speed
+	var min_angle = rotation
+	#angle_to_point returns the angle between the line connecting the two points and the X axis, in radians.
+	var max_angle = velocity.angle_to_point(Vector2.ZERO) - PI/2
+	rotation = lerp_angle(min_angle, max_angle,0.4)
+	
+	#disable it and set the collision layer of the physics layer from the tileset back to 1
+	torus_world()
+	
+	#apperantly delta is automatically applied in move and slide?
+	
 	move_and_slide()
+
+
+func torus_world():
+	if position.x < 0:
+		position.x = screen_size.x
+	if position.x > screen_size.x:
+		position.x = 0
+	if position.y < 0:
+		position.y = screen_size.y
+	if position.y > screen_size.y:
+		position.y = 0
--- world.gd
+++ world.gd
@@ -1,9 +1,10 @@
 extends Node2D
+@onready var tile_map = $TileMap
 
 var boid_scene: PackedScene = preload("res://boid.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
